96d64f47043032850e9bdb32e58c4be35767c0d0
Parker <contact@pkrm.dev>
2024-02-25 18:13:27 -0600

    Schedule data removal + change WSGI server

--- app/linklogger.py
+++ app/linklogger.py
@@ -1,11 +1,19 @@
 from routes import app
 from db import init_db
-from hypercorn.config import Config
-from hypercorn.asyncio import serve
-import asyncio
+import waitress
+import threading
+import schedule
+import time
+
+from func.remove_old_data import remove_old_data
 
 if __name__ == '__main__':
     init_db()
-    config = Config()
-    config.bind =["0.0.0.0:5252"]
-    asyncio.run(serve(app, config))
+    thread = threading.Thread(target=waitress.serve, args=(app,), kwargs={'port': 5252})
+    thread.start()
+    print('Server running on port 5252. Healthy.')
+
+    schedule.every().day.at('00:01').do(remove_old_data)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
