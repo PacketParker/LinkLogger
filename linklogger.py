accf52ff72e484795faacd5298a2e1584787d164
Parker <contact@pkrm.dev>
2024-11-05 08:12:42 -0600

    Create DB tables on start

--- linklogger.py
+++ linklogger.py
@@ -2,7 +2,10 @@ import uvicorn
 
 import config
 from app.main import app
+from database import Base, engine
 
+
+Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     config.load_config()
