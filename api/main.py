386e6dcb35c866d204e2d32fd814aa7ca3ed15ae
Parker <contact@pkrm.dev>
2024-11-11 20:45:52 -0600

    Remove origins

--- api/main.py
+++ api/main.py
@@ -25,17 +25,8 @@ app = FastAPI(
     },
 )
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "localhost:3000",
-    "127.0.0.1:3000",
-    # f"{CUSTOM_DOMAIN}"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
