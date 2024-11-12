7736846d86b0868bb86e51ae84970be980eca020
Parker <contact@pkrm.dev>
2024-11-11 23:59:23 -0600

    Run on localhost

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // Run on 127.0.0.1
+  // Proxy API requests to the backend
   server: {
-    host: '127.0.0.1',
     proxy: {
       '/api': 'http://127.0.0.1:5252',
     },
